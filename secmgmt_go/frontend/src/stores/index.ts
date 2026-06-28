7bf5174f423c4082c7791c0990204908548507d1
18070928015 <uvid@qq.com>
2026-06-28 17:33:04 +0800

    Update 2026-06-28 17:33:04

--- secmgmt_go/frontend/src/stores/index.ts
+++
@@ -1,8 +0,0 @@
-import { createPinia } from 'pinia'
-
-export { useAppStore } from './app'
-export { useAuthStore } from './auth'
-export { usePermissionStore } from './permission'
-export { useRealtimeStore } from './realtime'
-
-export const pinia = createPinia()
--- secmgmt_go/frontend/src/utils/permission.ts
+++
@@ -1,6 +0,0 @@
-import { pinia, usePermissionStore } from "../stores"
-
-export const hasPermission = (permissionCode: string): boolean => {
-  const permissionStore = usePermissionStore(pinia)
-  return permissionStore.hasPermission(permissionCode)
-}
